0c8eaa040fea3b084225b32d4a74abf0a2563f72
Soumitra2468 <sssoumitra412111221@gmail.com>
2025-04-06 14:25:00 +0530

    increment-decrement-counter-value

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -2,9 +2,31 @@ import { useState } from 'react'
 
 
 function App() {
+  let [counter, setCounter] = useState(0);
+  let increment = ()=>{
+    counter = counter + 1;
+    setCounter(counter);
+    console.log(counter);
+  }
+  let decremen = ()=>{
+    counter = counter - 1;
+    setCounter(counter);
+    console.log(counter);
+  }
+  let reset = ()=>{
+    counter = 0;
+    setCounter(counter);
+    console.log(counter);
+  }
   return (
     <>
         <h1>Hello Soumitra! You Are In React</h1>
+        <h2>Counter value: {counter}</h2>
+        <button onClick={increment}>Increment = {counter}</button>
+        <button onClick={decremen}>Decrement = {counter}</button>
+        <button onClick={reset}>Reset = {counter}</button>
+
+
     </>
   
   )
